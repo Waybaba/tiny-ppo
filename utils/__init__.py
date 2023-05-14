3a9a0198ae7f561159e4c2bbd657dc1ae9e3bbfe
waybaba <15305665971@163.com>
2023-05-14 08:28:13 +0000

    global cfg bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -32,7 +32,7 @@ from pytorch_lightning.utilities.logger import (
 )
 
 def make_env(env_cfg):
-    env = gym.make(env_cfg.name)
+    env = gym.make(env_cfg.name, use_contact_forces=env_cfg.use_contact_forces)
     env = DelayedRoboticEnv(env, env_cfg.delay, env_cfg.fixed_delay, env_cfg.global_cfg)
     if env_cfg.sticky_action_prob:
         env = StickyActionWrapper(env, env_cfg.sticky_action_prob)
