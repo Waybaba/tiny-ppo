5e7e72e9319ebf8dac15c22c6176a9299bb920e1
waybaba <wwang828@uwo.ca>
2023-08-09 02:24:34 -0400

    docker in china

--- src/launch_amlt.py
+++ src/launch_amlt.py
@@ -96,7 +96,7 @@ target:
     name: msrresrchvc
 
 environment:
-    image: waybaba/rl:v3
+    image: waybaba/rl:v3 # ! may need to be changed
     username: waybaba
     setup:
         - echo "setup start..."
