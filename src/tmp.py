01ad81a4e2b6c93ddec9a6e4275c5351d1b4483d
waybaba <wwang828@uwo.ca>
2023-11-21 16:20:33 +0000

    1

--- src/tmp.py
+++ src/tmp.py
@@ -1,38 +1,17 @@
-import gym_cityflow
 import gym
-import sys
-import os
-import random
+import time
 
-env = gym.make(
-    "cityflow-v0",
-    configPath="./output/gym-cityflow/tests/sample_data/sample_config.json",
-    episodeSteps=3600,
-)
+def run():
+    env = gym.make('gym_anm:ANM6Easy-v0')
+    o = env.reset()
 
-# Check action space
-print(env.action_space)
-print(env.observation_space)
-env.reset()
+    for i in range(100):
+        a = env.action_space.sample()
+        o, r, done, info = env.step(a)
+        env.render()
+        time.sleep(0.5)  # otherwise the rendering is too fast for the human eye.
 
-# disable print temporarily
-# iterate environment a lttle bit to test env
-actionInterval = 10
+    env.close()
 
-
-for i in range(10):
-    if i % actionInterval == 0:
-        testAction = []
-        for i in range(0, 16):
-            n = random.randint(0, 8)
-            testAction.append(n)
-    observation, reward, done, debug = env.step(action=testAction)
-    if done:
-        break
-
-observation, reward, done, debug = env.step(action=testAction)
-print(observation)
-print(reward)
-
-observation = env.reset()
-print(observation)
+if __name__ == '__main__':
+    run()
